649443ba5bd21577d7138211e5594951986527d5
Mateusz Spiewak <spiewaak9@gmail.com>
2021-02-22 22:29:09 +0100

    v1.2.1

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
-class GameStatus{
-  savedState = { //saveModel
+class GameStatus {
+  fameShop = { //! stworzyć pusty model, setItem JSON.stringfy(obiekt), var obiekt = getItem(obiekt)
+    fans: localStorage.getItem("fameshop.fans"),
+    groupies: localStorage.getItem("groupies"),
+    managers: localStorage.getItem("managers"),
+  };
+  moneyShop = {
+    djSetSwitches: localStorage.getItem("djSetSwitches"),
+    vinyl: localStorage.getItem("vinyl"),
+    silverVinyl: localStorage.getItem("silverVinyl"),
+    goldVinyl: localStorage.getItem("goldVinyl"),
+  };
+  gameDetails = {
     fame: localStorage.getItem('fame'),
     money: localStorage.getItem('money'),
-    fameShop: {
-      fans: localStorage.getItem('fans'),
-      groupies: localStorage.getItem('groupies'),
-      managers: localStorage.getItem('managers'),
-    },
-    moneyShop: {
-      djSetSwitches: localStorage.getItem('djSetSwitches'),
-      vinyl: localStorage.getItem('vinyl'),
-      silverVinyl: localStorage.getItem('silverVinyl'),
-      goldVinyl: localStorage.getItem('goldVinyl')
-    },
     totalClicks: localStorage.getItem('totalClicks'),
     timePlayed: localStorage.getItem('timePlayed')
+  }
+  savedState = {
+    fameshop: this.fameShop,
+    moneyShop: this.moneyShop,
+    gameDetails: this.gameDetails,
   };
 
-  newGame = () => {
-    Object.keys(this.savedState).forEach(v => localStorage.setItem(v,0)) //? generate empty, but nested object
-    console.log(this.savedState.fameShop.fans);
-    // this.fameShop = {
-    //   fans: localStorage.setItem('fans',1),
-    //   groupies: localStorage.setItem('groupies',0),
-    //   managers: localStorage.setItem('managers',0),
-    // }
-    // this.moneyShop = {
-    //   djSetSwitches: localStorage.setItem('djSetSwitches',0),
-    //   vinyl: localStorage.setItem('vinyl',0),
-    //   silverVinyl: localStorage.setItem('silverVinyl',0),
-    //   goldVinyl: localStorage.setItem('goldVinyl',0)
-    // }
+  saveObject = () => {
+    localStorage.setItem("SavedState", JSON.stringify(this.savedState));
   }
+
+  newGame = () => {
+    Object.keys(this.fameShop).forEach((v) => (this.fameShop[v] = 0));
+    Object.keys(this.moneyShop).forEach((v) => (this.moneyShop[v] = 0));
+    Object.keys(this.gameDetails).forEach((v) => (this.gameDetails[v] = 0));
+    this.fameShop.fans = 1; 
+    this.moneyShop.djSetSwitches = 1;
+    console.log(this.savedState);
+    this.saveObject();
+  };
 }
-class Main{
+class Main {
   constructor() {
     this.gameStatus = new GameStatus();
   }
@@ -48,19 +51,21 @@ class Main{
     return null;
   };
   addFame = () => {
-    fame=parseInt(fame)+parseInt(this.gameStatus.fameShop.fans);
-    console.log("fame: " +fame);
+    fame = parseInt(this.gameStatus.fameShop.fans);
+    console.log("fame: " + fame);
   };
   addMoney = () => {
-    money=parseInt(moneyPerClick)+moneyShop;
+    money = parseInt(moneyPerClick) + moneyShop;
     console.log("money: " + money);
   };
 }
+
 const game = new GameStatus();
 const main = new Main();
 
-if (main.readCookie("fame") === null && main.readCookie("money") === null) {
+if (localStorage.getItem("SavedState") === null) {
   game.newGame();
+  console.log("Started new game!");
 }
 
 var money = main.readCookie("money");
@@ -68,20 +73,21 @@ var fame = main.readCookie("fame");
 
 test = () => {
   main.addFame();
+};
+
+save = () => {
+  game.saveObject();
 }
+
+
 
 changeStage = () => {
   if (document.getElementById("mainStage").classList.contains("fame-div")) {
-    console.log(document.getElementById("mainStage").classList);
     document.getElementById("mainStage").classList.toggle("money-div");
   } else {
-    console.log(document.getElementById("mainStage").classList);
-    document.getElementById("mainStage").classList.toggle("fame-div")
+    document.getElementById("mainStage").classList.toggle("fame-div");
   }
 };
-
-
-
 
 // increaseValue = () => {
 //   if ((document.getElementById("mainStage").classList.contains("money-div"))) addMoney();
@@ -92,6 +98,3 @@ changeStage = () => {
 //   document.cookie = "fame=" + fame;
 //   document.cookie = "money=" + money;
 // };
-
-
-
